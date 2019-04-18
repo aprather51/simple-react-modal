fd4142957eb72dc5041ffdcbe0ee032534e64627
Arthur Prather <apratherjr@gmail.com>
2019-04-18 15:22:34 -0500

    update style and layouts

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Modal = ({ children, handleClose, show }) => {
-  const showHideClassName = show ? 'modal show' : 'modal hide';
+	const showHideClassName = show ? 'modal show' : 'modal hide';
 
-  return (
-    <div className={showHideClassName}>
-      <div className="btn close" onClick={handleClose}>
-        Close
-      </div>
-      <div className="modal-main">{children}</div>
-    </div>
-  );
+	return (
+		<div className={showHideClassName}>
+			<div className="container">
+				<div className="btn close" onClick={handleClose}>
+					Close
+				</div>
+				<div className="modal-main">{children}</div>
+			</div>
+		</div>
+	);
 };
 
 export default Modal;
